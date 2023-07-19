7b953c3b62f2bfb5f03e86cce50ecac82779fad2
FILIMON <f.rykov@bk.ru>
2023-07-19 17:58:38 +0300

    changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,13 +7,23 @@ const server = http.createServer();
 
 server.on('request', (req, res) => {
     if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<h1>HomePage</h1>');
-        res.end();
+        res.writeHead(200, {'Content-Type': 'text/html'})
+
+        res.write(
+            '<h1>HomePage</h1>'
+            )
+
+
+        res.end()
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.write('<h1>404 Not Found</h1>');
-        res.end();
+        res.writeHead(404, {
+            'Content-Type': 'text/html' 
+        })
+
+        res.write(
+            '<h1>404 Not Found</h1>'
+            )
+        res.end()
     }
 });
 
