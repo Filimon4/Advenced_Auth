fe0243f906f717489f31268706b8ef8e93c1945d
FILIMON <f.rykov@bk.ru>
2023-07-19 17:57:39 +0300

    init

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 5050;
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>HomePage</h1>');
+        res.end();
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write('<h1>404 Not Found</h1>');
+        res.end();
+    }
+});
+
+server.listen(PORT);
