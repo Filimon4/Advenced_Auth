d5f77e93f996d02ba1379555aabd7f8eeeaee66a
FILIMON <f.rykov@bk.ru>
2023-07-21 17:00:19 +0300

    Init project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,9 @@
-const x = 1;
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5050;
+const server = http.createServer();
+
+server.listen(PORT);
