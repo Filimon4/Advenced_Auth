251bd0bed39d94265ffc57ee8f0de9e50a526b20
FILIMON <f.rykov@bk.ru>
2023-07-25 23:03:30 +0300

    login

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -3,6 +3,10 @@ import bcrypt from "bcrypt";
 import UserModel from "../Schema/User.js"
 import { validationResult } from "express-validator";
 
+function generateAccessToken(id: string) {
+    const payload = {id}
+    return jwt.sign(payload, "SERCREAT_KEY", {expiresIn: "24h"})
+}
 
 class Auth {
     // @ts-ignore
@@ -30,7 +34,17 @@ class Auth {
     // @ts-ignore
     async login(req, res) {
         try {
-            
+            const {username, password} = req.body;
+            const user = await UserModel.findOne({username});
+            if (!user) {
+                return res.status(400).json({message: `There is not user with this username ${username}`})
+            }
+            const validPassword = bcrypt.compareSync(password, user.password!)
+            if (!validPassword) {
+                return res.status(400).json({message: "The password is incorrect"})
+            }
+            const jwtAccesToken = generateAccessToken(user._id.toString())
+            return res.json({jwtAccesToken})
         } catch (error) {
             console.log(error)
             res.status(400).json({message: error})
